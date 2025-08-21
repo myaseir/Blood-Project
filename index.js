ef9906db668213078a47a6d271ea3478027bd6ab
Muhammad Yasir <100397581+myaseir@users.noreply.github.com>
2025-08-21 11:13:18 +0500

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-donBlood = document.getElementById("donate")
-donBlood.addEventListener("click", donateFun)
-function donateFun(){
-    console.log("Donating Blood...");
-    document.getElementById("inputData").style.opacity = "1";
-    
-}
